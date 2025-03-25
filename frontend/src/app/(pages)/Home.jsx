07088fe14b4cb972a7ad4f1a07073675100d8fd9
TarushS <tsonakya@gmail.com>
2025-03-25 19:06:15 +0000

    folder layout

---
+++ frontend/src/app/(pages)/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EventSchd from '../_components/EventSchedule/EventSchedule'
+
+const HomePage = () => {
+  return (
+    <div className="flex flex-col space-y-4 p-4">
+    <text>haloo</text> 
+    <EventSchd />
+    </div>
+  )
+}
+
+export default HomePage
---
+++ frontend/src/app/_components/EventSchedule/EventSchedule.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventSchedule = () => {
+  return (
+    <text>EventSchedule hai bhai EventSchedule</text>
+  )
+}
+
+export default EventSchedule
