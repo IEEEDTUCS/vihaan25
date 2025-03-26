41c189f84b5507a82e64366235e116488f1faf97
Zahanboi <zahansharma123@gmail.com>
2025-03-26 20:44:14 +0530

    about vihaan

---
+++ frontend/src/app/(pages)/AboutVihaan/page.jsx
@@ -0,0 +1,11 @@
+import About from "@components/AboutVihaan/About"
+const page = () => {
+  return (
+    <>
+    {/* PAGES ARE JUST TO TEST COMPONENTS */}
+    <About/>
+    </>
+  );
+};
+
+export default page;
---
+++ frontend/src/app/_components/AboutVihaan/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './About.css';
+
+
+const About = () => {
+    return (
+      <div className="box">
+        
+        <div className='img'>
+          
+        <img src="/Images-Abt-IEEE/svg1.svg" alt="IMG-1" />
+        <img src="/Images-Abt-IEEE/svg2.svg" alt="IMG-1" />
+        <img src="/Images-Abt-IEEE/svgglass.svg" alt="IMG-1" />
+
+        </div>
+
+        <div className='head'><h1>Vihaan 8.0 <br /> Presented By IEEE dtu <br /></h1></div>
+        <div className='para'><p>Welcome to the About page of Vihaan.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit tenetur at facilis totam officia laudantium laboriosam sequi itaque, aliquid ut voluptatem ratione ipsa, laborum, doloremque nesciunt vero possimus quia est.
+        </p></div>
+        
+      </div>
+    );
+  };
+  
+  export default About;
+  
