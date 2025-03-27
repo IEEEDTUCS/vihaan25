a3c8ebc7a8610f521ec0953e547e548d065b5e42
unnat7779 <agrawalunnat7779@gmial.com>
2025-03-27 15:15:36 +0530

    add Navigation_links component to Home page and update Navbar song status display

--- frontend/src/app/(pages)/Home.jsx
+++ frontend/src/app/(pages)/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../_components/Navbar'
+import Navigation_links from '../_components/Navigation_links'
 const HomePage = () => {
   return (
     <>
     <Navbar/>
-
+    <Navigation_links/>
     </>
   )
 }
--- frontend/src/app/_components/Navbar.jsx
+++ frontend/src/app/_components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           {/* Logo Area */}
           <div className="flex space-x-1 items-center relative">
             <div className="text-center justify-center text-stone-500 text-xs font-normal font-['Orbitron'] tracking-widest">VOLUME</div>
-            <div className="text-center justify-center text-stone-500 text-xs font-normal font-['Orbitron'] tracking-widest">{songPlaying ? songName : '</>'}</div>
+            <div className="text-center justify-center text-stone-500 text-xs font-normal font-['Orbitron'] tracking-widest">{songPlaying ? '<ON>' : '<OFF>'}</div>
             <div style={{ width: '100%', height: '100%' , marginLeft:160,transform: 'rotate(180deg)' ,marginTop:20 }}>
                 <div style={{ width: 19, height: 0, left: 141, top: 19, position: 'absolute', transform: 'rotate(270deg)', transformOrigin: 'top left', outline: '1.50px #8F7E77 solid', outlineOffset: '-0.75px' }}></div>
                 <div style={{ width: 9, height: 9, left: 131, top: 14, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', borderRadius: 9999, border: '1.12px #8F7E77 solid' }} />
