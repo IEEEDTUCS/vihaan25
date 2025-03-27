3aafba1ab238deba602b70026c818edbcf9c1d94
TarushS <tsonakya@gmail.com>
2025-03-27 18:19:24 +0000

    DotButton added

--- frontend/src/app/(pages)/EventSchedule/page.jsx
+++ frontend/src/app/(pages)/EventSchedule/page.jsx
@@ -1,8 +1,10 @@
+import DotButton from "@/app/_components/Button";
 import EventSchedule from "@components/EventSchedule/EventSchedule";
 
 const page = () => {
   return (
     <>
+    <DotButton href="https://example.com">hallo vai mic testing</DotButton>
     {/* PAGES ARE JUST TO TEST COMPONENTS */}
     <EventSchedule/>
     </>
