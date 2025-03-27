1b21302f1666a8014e782ed1251cd37c7f93ff90
unnat7779 <agrawalunnat7779@gmial.com>
2025-03-27 18:17:26 +0530

    add Countdown component and integrate framer-motion and daisyui dependencies

--- frontend/src/app/(pages)/Home.jsx
+++ frontend/src/app/(pages)/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from '../_components/Navbar'
 import Navigation_links from '../_components/Navigation_links'
+import Countdown from '../_components/Countdown'
 const HomePage = () => {
   return (
     <>
     <Navbar/>
     <Navigation_links/>
+    <Countdown/>
+    
+
     </>
   )
 }
