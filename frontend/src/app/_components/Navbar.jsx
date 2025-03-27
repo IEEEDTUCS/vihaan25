40e3160e278879d3c5164cc2c39d17f35751d04b
unnat7779 <agrawalunnat7779@gmial.com>
2025-03-27 15:54:35 +0530

    update folder structure and improve asset organization

--- frontend/src/app/_components/Navbar.jsx
+++ frontend/src/app/_components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Menu, X } from 'lucide-react';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [songPlaying,setSongPlaying]= useState(false);
+  const [songPlaying, setSongPlaying] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -18,55 +18,57 @@ const Navbar = () => {
           <div className="flex space-x-1 items-center relative">
             <div className="text-center justify-center text-stone-500 text-xs font-normal font-['Orbitron'] tracking-widest">VOLUME</div>
             <div className="text-center justify-center text-stone-500 text-xs font-normal font-['Orbitron'] tracking-widest">{songPlaying ? '<ON>' : '<OFF>'}</div>
-            <div style={{ width: '100%', height: '100%' , marginLeft:160,transform: 'rotate(180deg)' ,marginTop:20 }}>
-                <div style={{ width: 19, height: 0, left: 141, top: 19, position: 'absolute', transform: 'rotate(270deg)', transformOrigin: 'top left', outline: '1.50px #8F7E77 solid', outlineOffset: '-0.75px' }}></div>
-                <div style={{ width: 9, height: 9, left: 131, top: 14, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', borderRadius: 9999, border: '1.12px #8F7E77 solid' }} />
-                <div style={{ width: 15, height: 0, left: 123, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 15, height: 0, left: 58, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 5, height: 0, left: 102, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 5, height: 0, left: 37, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 15, height: 0, left: 91, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 15, height: 0, left: 26, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 9, height: 0, left: 74, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 9, height: 0, left: 9, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-              </div>
+            <div style={{ width: '100%', height: '100%', marginLeft: 160, transform: 'rotate(180deg)', marginTop: 20 }}>
+              <div style={{ width: 19, height: 0, left: 141, top: 19, position: 'absolute', transform: 'rotate(270deg)', transformOrigin: 'top left', outline: '1.50px #8F7E77 solid', outlineOffset: '-0.75px' }}></div>
+              <div style={{ width: 9, height: 9, left: 131, top: 14, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', borderRadius: 9999, border: '1.12px #8F7E77 solid' }} />
+              <div style={{ width: 15, height: 0, left: 123, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 15, height: 0, left: 58, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 5, height: 0, left: 102, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 5, height: 0, left: 37, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 15, height: 0, left: 91, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 15, height: 0, left: 26, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 9, height: 0, left: 74, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 9, height: 0, left: 9, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+            </div>
           </div>
 
           {/* Navigation Links */}
-          <div className="hidden md:block">
-            <div className=" flex items-baseline lg:-right-[28%]    relative " >
-              {/* Dotted Lines */}
-              <div style={{ width: '100%', height: '100%' , marginRight:160  }}>
-                <div style={{ width: 19, height: 0, left: 141, top: 19, position: 'absolute', transform: 'rotate(270deg)', transformOrigin: 'top left', outline: '1.50px #8F7E77 solid', outlineOffset: '-0.75px' }}></div>
-                <div style={{ width: 9, height: 9, left: 131, top: 14, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', borderRadius: 9999, border: '1.12px #8F7E77 solid' }} />
-                <div style={{ width: 15, height: 0, left: 123, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 15, height: 0, left: 58, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 5, height: 0, left: 102, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 5, height: 0, left: 37, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 15, height: 0, left: 91, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 15, height: 0, left: 26, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 9, height: 0, left: 74, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-                <div style={{ width: 9, height: 0, left: 9, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
-              </div>
 
-              <div className="flex justify-evenly space-x-2 border-white  ">
+          <div className=" flex items-baseline  -right-[12vw]    relative " >
+            {/* Dotted Lines */}
+            <div style={{ width: '100%', height: '100%', marginRight: 160 }}>
+              <div style={{ width: 19, height: 0, left: 141, top: 19, position: 'absolute', transform: 'rotate(270deg)', transformOrigin: 'top left', outline: '1.50px #8F7E77 solid', outlineOffset: '-0.75px' }}></div>
+              <div style={{ width: 9, height: 9, left: 131, top: 14, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', borderRadius: 9999, border: '1.12px #8F7E77 solid' }} />
+              <div style={{ width: 15, height: 0, left: 123, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 15, height: 0, left: 58, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 5, height: 0, left: 102, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 5, height: 0, left: 37, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 15, height: 0, left: 91, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 15, height: 0, left: 26, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 9, height: 0, left: 74, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+              <div style={{ width: 9, height: 0, left: 9, top: 10, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: 'top left', outline: '1px #8F7E77 solid', outlineOffset: '-0.50px' }}></div>
+            </div>
+
+            <div className="flex justify-evenly space-x-4 border-white  ">
               <div style={{ textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#8F7E77', fontSize: 12, fontFamily: 'Orbitron', fontWeight: '400', letterSpacing: 2.40, wordWrap: 'break-word' }}>TEAM</div>
               <div style={{ textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#8F7E77', fontSize: 12, fontFamily: 'Orbitron', fontWeight: '400', letterSpacing: 2.40, wordWrap: 'break-word' }}>JUDGES</div>
-              
+
               <div style={{ textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#8F7E77', fontSize: 12, fontFamily: 'Orbitron', fontWeight: '400', letterSpacing: 2.40, wordWrap: 'break-word' }}>FAQ</div>
+              <div style={{ textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#8F7E77', fontSize: 12, fontFamily: 'Orbitron', fontWeight: '400', letterSpacing: 2.40, wordWrap: 'break-word' }}>TRACK</div>
               <div  style={{ textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#8F7E77', fontSize: 12, fontFamily: 'Orbitron', fontWeight: '400', letterSpacing: 2.40, wordWrap: 'break-word' }}>TRACK</div>
-              <div  style={{ textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#8F7E77', fontSize: 12, fontFamily: 'Orbitron', fontWeight: '400', letterSpacing: 2.40, wordWrap: 'break-word' }}>TRACK</div>
-              <div  style={{ textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#8F7E77', fontSize: 12, fontFamily: 'Orbitron', fontWeight: '400', letterSpacing: 2.40, wordWrap: 'break-word' }}>TRACK</div>
-              <div  style={{ textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#8F7E77', fontSize: 12, fontFamily: 'Orbitron', fontWeight: '400', letterSpacing: 2.40, wordWrap: 'break-word' }}>TRACK</div>
-              <div >
-                <img src="/side.png" alt="Navbar Side" className="absolute mt-1   z-10" />
+              {/* <div  style={{ textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#8F7E77', fontSize: 12, fontFamily: 'Orbitron', fontWeight: '400', letterSpacing: 2.40, wordWrap: 'break-word' }}>TRACK</div> */}
+              {/* <div  style={{ textAlign: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', color: '#8F7E77', fontSize: 12, fontFamily: 'Orbitron', fontWeight: '400', letterSpacing: 2.40, wordWrap: 'break-word' }}>TRACK</div> */}
+              <div className='-right-[1vw] absolute top-2 '>
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="599" viewBox="0 0 22 599" fill="none">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M22.0004 1V1.875V12.8605H21.9847V13H20.9847V1.875H0.000366211V0.875H22.0004V1ZM22.0004 42.5124V18.7908H21.0004L21.0004 42.5124H22.0004ZM22.0004 58.3264V99.8386H21.0004L21.0004 58.3264H22.0004ZM22.0004 151.235V113.676H21.0004L21.0004 151.235H22.0004ZM22.0004 157.165V204.608H21.0004L21.0004 157.165H22.0004ZM22.0004 222.398V208.562H21.0004V222.398H22.0004ZM22.0004 228.328V256.004H21.0004L21.0004 228.328H22.0004ZM22.0004 275.772V259.958H21.0004V275.772H22.0004ZM22.0004 281.702V293.562H21.0004V281.702H22.0004ZM22.0004 315.306V297.516H21.0004L21.0004 315.306H22.0004ZM22.0004 319.26V348.912H21.0004L21.0004 319.26H22.0004ZM22.0004 366.702V350.888H21.0004V366.702H22.0004ZM22.0004 370.656V372.634H21.0004V370.656H22.0004ZM22.0004 390.424V378.564H21.0004L21.0003 390.424H22.0004ZM22.0004 400.308V447.75H21.0003L21.0003 400.308H22.0004ZM22.0004 467.518V449.728H21.0003V467.518H22.0004ZM22.0004 471.472V479.378H21.0003V471.472H22.0004ZM21.0003 598.858H22.0004V483.332H21.0003L21.0003 598.858Z" fill="#8F7E77" />
+                </svg>
               </div>
-              </div>
-              
-              
             </div>
-            
+
+
           </div>
+
+
 
           {/* Mobile Menu Button */}
           <div className="-mr-2 flex md:hidden">
