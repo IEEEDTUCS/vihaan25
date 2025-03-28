4d1a6c234c6e24ce76c86ee7ee04815e0d96099a
AbhimanyuKumarJha <jha.abhi1610@gmail.com>
2025-03-29 00:47:10 +0530

    Currently changing in about

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,13 +1,17 @@
 import HomePage from "./(pages)/Home";
+import About from "./_components/AboutVihaan/About";
 import EventSchedule from "./_components/EventSchedule/EventSchedule";
 import FAQ from "./_components/FAQ's/faq";
+import Footer from "./_components/Footer/Footer";
 
 export default async function Home() {
   return (
     <>
       <HomePage/>
+      <About/>
        <EventSchedule/>
       <FAQ/>
+      <Footer/> 
     </>
   );
 }
