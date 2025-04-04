009f23c004c89d040ab1c82472c84ed663c2ca48
AbhimanyuKumarJha <jha.abhi1610@gmail.com>
2025-04-04 12:10:45 +0530

    rerender optimise

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,8 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./_components/NavBar/Navbar";
-import VerticleLines from "@/app/assets/VerticleLines.svg";
-import Image from "next/image";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,11 +23,6 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {/* <Image
-            src={VerticleLines}
-            alt="Verticle Lines"
-            className="absolute top-[50] self-center  -z-10 Verticle Lines"
-          /> */}
         {children}
       </body>
     </html>
