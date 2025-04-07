584bc3575c090e38c921b254b59aa9d372f29bd6
AbhimanyuKumarJha <jha.abhi1610@gmail.com>
2025-04-07 13:59:01 +0530

    overflow-about-fix

--- frontend/src/app/_components/AboutVihaan/About.jsx
+++ frontend/src/app/_components/AboutVihaan/About.jsx
@@ -1,17 +1,14 @@
-"use client"
+"use client";
 import React from "react";
 import Button from "@/app/_components/ui/Button";
 // import "./About.css";
 import "@/app/globals.css";
 import AboutSVG from "./AboutSvg";
 import { useState, useEffect } from "react";
-import AbtIEEE from "./About-IEEE-Nav/IEEE"
+import AbtIEEE from "./About-IEEE-Nav/IEEE";
 import { motion } from "framer-motion";
 
- 
-
 const About = () => {
-
   const [className, setClassName] = useState("");
   const [isBlurred, setIsBlurred] = useState(false);
   const [isHidden, setIsHidden] = useState(true);
@@ -19,15 +16,14 @@ const About = () => {
   const popout = () => {
     setClassName("Button-for-Abt-Section");
     setIsBlurred(true);
-    setIsHidden(false); 
+    setIsHidden(false);
   };
-
 
   const handleImageClick = (e) => {
     e.target.classList.add("animate-wiggle");
     setTimeout(() => {
       e.target.classList.remove("animate-wiggle");
-    }, 4000); 
+    }, 4000);
   };
 
   return (
@@ -39,57 +35,70 @@ const About = () => {
       />
       {/* Ye SVG ka hai */}
 
-      <motion.div 
-      className="z-10 w-[250%] m-6 text-wrap sm:mx-10 md:mx-14 lg:mx-20 max-[1280px]:relative max-[1280px]:w-full ">
+      <motion.div className="z-10 w-[250%] m-6 text-wrap sm:mx-10 md:mx-14 lg:mx-20 max-[1280px]:relative max-[1280px]:w-full ">
         <motion.div
-        whileInView={{x:0, opacity:1}}
-        initial={{opacity:0, x:-100}}
-        transition={{duration:1}}
-        viewport={{ once: true }}
-        className="mb-10 mr-10 ml-0 w-150 max-[750px]:w-[150%] max-[750px]:h-28 max-[750px]:relative flex  ">
-          <img src="/Images-Abt-IEEE/head.svg" alt="Heading" className="w-full max-[750px]:relative max-[750px]:left-0 max-[750px]:w-[60%] max-[750px]:top-[40%]" />
+          whileInView={{ x: 0, opacity: 1 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+          className="mb-10 mr-10 ml-0 w-150 max-[750px]:w-[150%] max-[750px]:h-28 max-[750px]:relative flex  "
+        >
+          <img
+            src="/Images-Abt-IEEE/head.svg"
+            alt="Heading"
+            className="w-full max-[750px]:relative max-[750px]:left-0 max-[750px]:w-[60%] max-[750px]:top-[40%]"
+          />
         </motion.div>
-        <motion.h1 
-        whileInView={{x:0, opacity:1}}
-        initial={{opacity:0, x:-100}}
-        transition={{duration:1}}
-        viewport={{ once: true }}
-        className="text-7xl tracking-widest max-[600px]:pr-[8%] text-[#A59188] text-heading font-khinterference xl:w-[80%] w-full max-[600px]:text-[300%] max-[600px]:font-bold max-[600px]:tracking-[0rem] ">
+        <motion.h1
+          whileInView={{ x: 0, opacity: 1 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+          className="text-7xl tracking-widest max-[600px]:pr-[8%] text-[#A59188] text-heading font-khinterference xl:w-[80%] w-full max-[600px]:text-[300%] max-[600px]:font-bold max-[600px]:tracking-[0rem] "
+        >
           VIHAAN 8.0
         </motion.h1>
-        <motion.h2 
-        whileInView={{x:0, opacity:1}}
-        initial={{opacity:0, x:-100}}
-        transition={{duration:1}}
-        viewport={{ once: true }}
-        className="text-6xl text-[#A59188] max-[600px]:pr-[8%] xl:w-[120%] font-medium text-heading tracking-wide font-khinterference mt-2 max-[600px]:text-[200%] ;">
+        <motion.h2
+          whileInView={{ x: 0, opacity: 1 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+          className="text-6xl text-[#A59188] max-[600px]:pr-[8%] xl:w-[120%] font-medium text-heading tracking-wide font-khinterference mt-2 max-[600px]:text-[200%] ;"
+        >
           PRESENTED BY IEEE DTU
         </motion.h2>
-        <motion.p 
-        whileInView={{x:0, opacity:1}}
-        initial={{opacity:0, x:-100}}
-        transition={{duration:1}}
-        viewport={{ once: true }}
-        className="text-para pr-[0] mt-4 text-justify xl:w-[100%] w-full font-orbitron font-normal tracking-wider max-[600px]:text-[50%] text-[#A59188] sm:tracking-widest max-[600px]:pr-[20%] max-[600px]:w-full max-[600px]:text-sm text-base ">
+        <motion.p
+          whileInView={{ x: 0, opacity: 1 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+          className="text-para pr-[0] mt-4 text-justify xl:w-[100%] w-full font-orbitron font-normal tracking-wider max-[600px]:text-[50%] text-[#A59188] sm:tracking-widest max-[600px]:pr-[20%] max-[600px]:w-full text-base "
+        >
           VIHAAN is a 24-hour long hackathon that provides a platform for
           budding programmers to come up with solutions to real-world problems
-          using technology. Students participate in teams of up to 4 members across various tracks. To
-          add to the experience and learning quotient, teams that make it past
-          the hackathon are awarded various cash prizes and goodies.
+          using technology. Students participate in teams of up to 4 members
+          across various tracks. To add to the experience and learning quotient,
+          teams that make it past the hackathon are awarded various cash prizes
+          and goodies.
         </motion.p>
-        <motion.div 
-        whileInView={{x:0, opacity:1}}
-        initial={{opacity:0, x:-100}}
-        transition={{duration:1}}
-        viewport={{ once: true }}
-        className="mt-6 w-fit relative left-[10%]" >
+        <motion.div
+          whileInView={{ x: 0, opacity: 1 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          viewport={{ once: true }}
+          className="mt-6 w-fit relative left-[10%]"
+        >
           <button onClick={popout}>
-          <Button href="" children={"About IEEE DTU"}/>
+            <Button href="" children={"About IEEE DTU"} />
           </button>
         </motion.div>
-          <AbtIEEE className={className} setClassName={setClassName} isHidden={isHidden} setIsHidden={setIsHidden} />
-        <div className="ml-[50%] relative -top-20 max-xl:collapse">
-        </div>
+        <AbtIEEE
+          className={className}
+          setClassName={setClassName}
+          isHidden={isHidden}
+          setIsHidden={setIsHidden}
+        />
+        <div className="ml-[50%] relative -top-20 max-xl:collapse"></div>
         <div className="ml-[60%] relative -top-20 max-xl:collapse">
           <AboutSVG />
         </div>
