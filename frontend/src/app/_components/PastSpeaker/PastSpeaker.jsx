2b8845a1e8ae86419cb35a5c2ad5fe10f2a6759e
Darsh Jain <132695189+sparky0520@users.noreply.github.com>
2025-04-10 20:10:12 +0530

    Added past speakers

---
+++ frontend/src/app/_components/PastSpeaker/PastSpeaker.jsx
@@ -0,0 +1,32 @@
+"use-client";
+import Image from "next/image";
+
+export default function PastSpeaker({
+  name,
+  title,
+  about,
+  linkedin,
+  url,
+  isActive,
+}) {
+  return (
+    <>
+      <div className="group relative h-[300px] w-[300px]">
+        <Image src={url} width={300} height={300} />
+        <div
+          className={`absolute  h-[300px] w-[300px] duration-300  bg-amber-300 p-8 group-hover:top-0 group-hover:opacity-100 ${
+            isActive ? "opacity-100 top-0" : "opacity-0 top-[100%]"
+          } pointer-events-none flex flex-col gap-4`}
+        >
+          <div>{name}</div>
+          <div>{title}</div>
+          <hr></hr>
+          <div>{about}</div>
+          <a href={linkedin}>
+            <Image src="past-speaker/linkedin.png" width={30} height={30} />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
