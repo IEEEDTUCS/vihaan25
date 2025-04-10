f31b6fb3c644fdb3bcd6ed7ebcdb2fecec3af433
Zahanboi <zahansharma123@gmail.com>
2025-04-10 17:55:26 +0530

    made text truncate to stop overflow

--- frontend/src/app/_components/Quiz/LeaderBoard.jsx
+++ frontend/src/app/_components/Quiz/LeaderBoard.jsx
@@ -24,56 +24,57 @@ export default function LeaderBoard() {
           </h1>
         </div>
 
+        {/* isse truncate ko use krna vrna text will overflow from the images and thia way people will also see the rankings */}
         {/* add dynamic winners ke naam */}
 
-        <div className="flex flex-wrap justify-start relative bordernew bg-contain bg-no-repeat bg-center w-[80%] h-[100%] pl-5 z-10">
-        <div className="bg-[url('/Quiz/1st-pos.svg')] bg-contain bg-no-repeat w-full h-[50%] flex items-center justify-center mt-4">
-                <p className="text-xl font-bold absolute left-100 max-[920px]:relative max-[920px]:left-20 border border-amber-950 text-[#4e3124] font-khinterference tracking-normal">
-                    Winner 1
+            <div className="flex flex-wrap justify-start relative bordernew bg-contain bg-no-repeat bg-center w-[80%] h-[100%] pl-5 z-10">
+                <div className="bg-[url('/Quiz/1st-pos.svg')] bg-contain bg-no-repeat w-full h-[50%] flex items-center justify-center mt-4">
+                <p className="text-xl font-bold absolute left-100 max-[920px]:relative max-[920px]:left-20 border border-amber-950 text-[#4e3124] font-khinterference tracking-normal text-center max-w-[10%] truncate">
+                Team-Crazy-as-Fuck(Winner-1)
                 </p>
-            </div>
-            <div className="bg-[url('/Quiz/2nd-pos.svg')] bg-contain bg-no-repeat w-[35%] h-[50%] max-[920px]:w-[55%] max-[1200px]:w-[45%] max-[920px]:h-[35%] flex items-center justify-center mt-4">
-                <p className="text-xl font-bold absolute bottom-43 border text-[#4e3124] border-amber-950 font-khinterference tracking-tight">
-                    Winner 2
+                </div>
+                <div className="bg-[url('/Quiz/2nd-pos.svg')] bg-contain bg-no-repeat w-[35%] h-[50%] max-[920px]:w-[55%] max-[1200px]:w-[45%] max-[920px]:h-[35%] flex items-center justify-center mt-4">
+                <p className="text-xl font-bold absolute bottom-38 border text-[#4e3124] border-amber-950 font-khinterference tracking-tight text-center max-w-[10%] truncate">
+                Team-Crazy-as-Fuck(Winner-2)
                 </p>
-            </div>
-            <div className="bg-[url('/Quiz/3rd-pos.svg')] bg-contain bg-no-repeat w-[30%] h-[50%] max-[920px]:w-[45%] max-[1200px]:w-[40%] max-[920px]:h-[35%] flex items-center justify-center mt-4">
-                <p className="text-xl font-bold absolute bottom-43 text-[#4e3124] border border-amber-950 font-khinterference tracking-tighter">
-                    Winner 3
+                </div>
+                <div className="bg-[url('/Quiz/3rd-pos.svg')] bg-contain bg-no-repeat w-[30%] h-[50%] max-[920px]:w-[45%] max-[1200px]:w-[40%] max-[920px]:h-[35%] flex items-center justify-center mt-4">
+                <p className="text-xl font-bold absolute bottom-38 text-[#4e3124] border border-amber-950 font-khinterference tracking-tighter text-center max-w-[10%] truncate">
+                Team-Crazy-as-Fuck(Winner-3)
                 </p>
+                </div>
             </div>
-        </div>
-      </div>
+              </div>
 
-     
-      <div className="hidden max-[650px]:flex flex-col items-center justify-start w-full h-full p-4">
-        
-        <div className="flex flex-col items-center py-4 z-10">
-          <img
-            src="/Quiz/results.svg"
-            alt="Quiz Results"
-            className="w-auto h-auto mb-2"
-          />
-          <h1 className="text-4xl tracking-widest text-[#9a857b] font-khinterference text-center">
-            TOP CONVICTS
-          </h1>
-        </div>
+             
+              <div className="hidden max-[650px]:flex flex-col items-center justify-start w-full h-full p-4">
+            
+            <div className="flex flex-col items-center py-4 z-10">
+              <img
+                src="/Quiz/results.svg"
+                alt="Quiz Results"
+                className="w-auto h-auto mb-2"
+              />
+              <h1 className="text-4xl tracking-widest text-[#9a857b] font-khinterference text-center">
+                TOP CONVICTS
+              </h1>
+            </div>
 
-{/* add dynamic winners ke naam */}
+        {/* add dynamic winners ke naam */}
        
          <div className="border-[15px] border-[url('/Quiz/wood-border.png')] bg-[#121212] w-full h-auto rounded-xl p-4 space-y-4">
                 <div className="relative bg-[url('/Quiz/1st-pos.svg')] bg-contain bg-no-repeat bg-center h-40 w-full flex items-center justify-center">
-                    <div className="text-[#4e3124] border-amber-950 font-khinterference tracking-normal text-base font-bold ml-23">Winner 1</div> 
+                    <div className="text-[#4e3124] border-amber-950 font-khinterference tracking-normal text-base font-bold ml-23 max-w-[15%] truncate">Winnddddddddddddddddddddddddddddddddddddder 1</div> 
                 </div>
 
             
                 <div className="relative bg-[url('/Quiz/2nd-pos.svg')] bg-contain bg-no-repeat bg-center h-40 w-full flex items-center justify-center">
-                    <div className="text-[#4e3124] border-amber-950 font-khinterference tracking-tight text-base font-bold">Winner 2</div>
+                    <div className="text-[#4e3124] border-amber-950 font-khinterference tracking-tight text-base font-bold max-w-[15%] truncate">Winnffffffffffffffffffffffffffffffffffffffffffer 2</div>
                 </div>
 
              
                 <div className="relative bg-[url('/Quiz/3rd-pos.svg')] bg-contain bg-no-repeat bg-center h-40 w-full flex items-center justify-center">
-                    <div className="text-[#4e3124] border-amber-950 font-khinterference tracking-tight text-base font-bold">Winner 3</div>
+                    <div className="text-[#4e3124] border-amber-950 font-khinterference tracking-tight text-base font-bold max-w-[15%] truncate">Winner 3</div>
                 </div>
             </div>
       </div>
