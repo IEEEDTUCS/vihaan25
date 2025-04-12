03b31d1f5cad23c1cb8455cfe5f9591583bd85ec
AbhimanyuKumarJha <jha.abhi1610@gmail.com>
2025-04-13 03:21:31 +0530

    Vercel Analtyics

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from "./_components/NavBar/Navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       >
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
